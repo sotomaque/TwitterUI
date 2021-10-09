8d23e8283666c089c820fa3f732b416287d30bdc
sotomaque <sotomaque@gmail.com>
2021-10-09 00:17:13 -0700

    Initial Commit

---
+++ TwitterUI/TwitterUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterUIApp.swift
+//  TwitterUI
+//
+//  Created by Enrique Sotomayor on 10/9/21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
